4ae6a4cfa3572abea6eb4832203cde1b3f80acb3
VALLE Benoit <benoit.valle@ca-assurances.fr>
2026-03-24 11:35:06 +0100

    version dep non hard codée

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     application
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    alias(libs.plugins.shadow)
 }
 
 group = "org.valle"
@@ -12,31 +12,31 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+    implementation(libs.jackson.databind)
+    implementation(libs.jackson.dataformat.yaml)
     // Utilisation de la version stable de Lanterna avec gui2 inclus
-    implementation("com.googlecode.lanterna:lanterna:3.1.2")
-    implementation("info.picocli:picocli:4.7.5")
-    implementation("org.slf4j:slf4j-api:2.0.13")
-    implementation("org.slf4j:slf4j-simple:2.0.13")
-    implementation("org.projectlombok:lombok:1.18.32")
+    implementation(libs.lanterna)
+    implementation(libs.picocli)
+    implementation(libs.slf4j.api)
+    implementation(libs.slf4j.simple)
+    implementation(libs.lombok)
     // https://mvnrepository.com/artifact/jakarta.validation/jakarta.validation-api
-    implementation("jakarta.validation:jakarta.validation-api:3.1.1")
+    implementation(libs.jakarta.validation.api)
     // https://mvnrepository.com/artifact/jakarta.el/jakarta.el-api
-    implementation("jakarta.el:jakarta.el-api:6.0.1")
+    implementation(libs.jakarta.el.api)
     // https://mvnrepository.com/artifact/jakarta.annotation/jakarta.annotation-api
-    implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
-    implementation("org.hibernate.validator:hibernate-validator:7.0.5.Final")
-    implementation("org.glassfish:jakarta.el:4.0.2") // nécessaire pour l’EL
+    implementation(libs.jakarta.annotation.api)
+    implementation(libs.hibernate.validator)
+    implementation(libs.glassfish.jakarta.el) // nécessaire pour l'EL
 
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+    annotationProcessor(libs.lombok)
+    annotationProcessor(libs.picocli.codegen)
 
-    testImplementation("org.assertj:assertj-core:3.25.3")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.mockito:mockito-core:5.11.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+    testImplementation(libs.assertj.core)
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.junit.jupiter)
 }
 
 tasks.test {
